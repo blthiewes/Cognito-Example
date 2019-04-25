b8e3d2b7598b50d6b24e6a9ba0d6f5e7b518895e
Brent <blthiewes@gmail.com>
2019-04-24 22:05:04 -0500

    commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cognito_example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private const string _clientId = "3vo7gfljfahvfcrnm4fqqnqd4i";
+        private readonly RegionEndpoint _region = RegionEndpoint.USEast2;
+
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Email { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Register(User user)
+        {
+            var cognito = new AmazonCognitoIdentityProviderClient(_region);
+
+            var request = new SignUpRequest
+            {
+                ClientId = _clientId,
+                Password = user.Password,
+                Username = user.Username
+            };
+
+            var emailAttribute = new AttributeType
+            {
+                Name = "email",
+                Value = user.Email
+            };
+            request.UserAttributes.Add(emailAttribute);
+
+            try
+            {
+                var response = await cognito.SignUpAsync(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace cognito_example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = "3vo7gfljfahvfcrnm4fqqnqd4i";
+                    options.Authority = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_YdqzmRDYB";
+                });
+
+
+            // Adds Amazon Cognito as Identity Provider
+            //services.AddCognitoIdentity();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // If not already enabled, you will need to enable ASP.NET Core authentication
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
